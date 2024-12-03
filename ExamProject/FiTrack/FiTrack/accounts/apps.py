cf7f2b1e498024588db62bc659777d9fcd9d2e87
Velimir-Dimitrov <dark7emplar@gmail.com>
2024-12-03 19:56:47 +0200

    extending the user model

--- ExamProject/FiTrack/FiTrack/accounts/apps.py
+++ ExamProject/FiTrack/FiTrack/accounts/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'FiTrack.accounts'
+
+    def ready(self):
+        import FiTrack.accounts.signals
--- ExamProject/FiTrack/FiTrack/accounts/views.py
+++ ExamProject/FiTrack/FiTrack/accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+class AppUserLoginView:
+    pass
+
+class AppUserRegisterView:
+    pass
+
+
+class ProfileDeleteView:
+    pass
+
+
+class ProfileDetailView:
+    pass
+
+
+class ProfileEditView:
+    pass
